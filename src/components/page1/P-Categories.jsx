e6ef60fd3093c6219e73d38a33eb68eea27d5161
Paullor08 <pongthum12246@gmail.com>
2024-08-03 21:16:47 +0700

    firstcommit

---
+++ src/components/page1/P-Categories.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import 'boxicons';
+export default function Categories() {
+
+    return (
+
+        <div id="CategoriesSection" className="h-[1024px] flex flex-col items-center relative">
+            <div className="w-[262px] h-[87px] flex flex-col items-center justify-center justify-between text-center">
+                <p className="font-bold text-2xl">Categories</p>
+                <p>find what are you looking for</p>
+            </div>
+
+            <div className="mt-[95px] h-[841px] w-full bg-[#C1DCDC] flex justify-center">
+
+                <div id="boxcontainer" className="flex space-x-[94px]">
+
+                    <div id="box1" className="mt-[45px] w-[352px] h-[692px] absolute left-5 top-[93px]">
+                        <div className="w-[352px] h-[512px] bg-gray-600">
+                            <img src="#"></img>
+                        </div>
+                        <div className="w-[352px] h-[27px] text-center space-y-3 mt-[21px]">
+                            <p className="font-extrabold text-xl">Plant Accessories</p>
+                        </div>
+                    </div>
+
+                    <div id="box2" className="mt-[45px] w-[352px] h-[692px] relative right-[40px]">
+                        <div className="w-[352px] h-[512px] bg-gray-600">
+                            <img src="#"></img>
+                        </div>
+                        <div className="w-[352px] h-[27px] text-center mt-[21px] space-y-2">
+                            <p className="font-extrabold text-xl">Plant Accessories</p>
+                            <p className="text-gray-500">Horem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            <button className="w-[147px] h-[51px] bg-white rounded-2xl font-semibold text-lg">Explore &rarr;</button>
+                        </div>
+                    </div>
+
+                    <div id="box3" className="mt-[45px] w-[352px] h-[692px] absolute right-5 top-[93px]">
+                        <div className="w-[352px] h-[512px] bg-gray-600">
+                            <img src="#"></img>
+                        </div>
+                        <div className="w-[352px] h-[27px] text-center mt-[21px]">
+                            <p className="font-extrabold text-xl">Plant Accessories</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
