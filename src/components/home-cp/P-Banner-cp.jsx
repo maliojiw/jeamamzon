0d0f224482d9e1c10c1282201659ae97a95415e1
Paullor08 <pongthum12246@gmail.com>
2024-08-08 19:54:44 +0700

    test

--- src/components/home-cp/P-Banner-cp.jsx
+++ src/components/home-cp/P-Banner-cp.jsx
@@ -21,21 +21,12 @@ export default function Banner_cp() {
                 </div>
                 <div class="relative ml-[48px] mt-[55px] w-[448px]">
                     <input
-                        class="h-[65px] w-[448px] rounded-2xl pl-[50px] font-semibold text-gray-500"
+                        class="h-[65px] w-[448px] rounded-2xl pl-[50px] font-semibold text-gray-500 bg-white"
                     />
                     <box-icon
                         name='search-alt'
                         class="absolute top-[50%] transform -translate-y-1/2 left-[400px]"
                     ></box-icon>
-                </div>
-                <div className="flex justify-end absolute right-[230.5px] bottom-[-64.5px]">
-                    <div className="w-[305px] h-[300px] bg-black rounded-full"></div>
-                </div>
-                <div className="flex justify-end absolute right-[230.5px] bottom-[-64.5px]">
-                    <div className="w-[235px] h-[100px] bg-black rounded-3xl"></div>
-                </div>
-                <div className="flex justify-end absolute right-[230.5px] bottom-[-64.5px]">
-                    <div className="w-[130px] h-[205px] bg-black rounded-3xl"></div>
                 </div>
             </div>
 
